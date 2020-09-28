eecc87a58523a197bc6ff902140b06b3c4e35819
Matthew Viola <matthew.viola@temple.edu>
2020-09-28 11:42:07 -0400

    Added code to coloradapter and created list in main

--- app/src/main/java/edu/temple/colorchangerapp/ColorActivity.java
+++ app/src/main/java/edu/temple/colorchangerapp/ColorActivity.java
@@ -6,12 +6,30 @@ import android.os.Bundle;
 import android.widget.Adapter;
 import android.widget.BaseAdapter;
 
+import java.util.ArrayList;
+
 public class ColorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> colors = new ArrayList<>();
+        colors.add("Black");
+        colors.add("Blue");
+        colors.add("Yellow");
+        colors.add("Magenta");
+        colors.add("Green");
+        colors.add("Cyan");
+        colors.add("Gray");
+        colors.add("Red");
+        colors.add("White");
+        colors.add("Dark Gray");
+
+        final BaseAdapter color = new ColorAdapter(this, colors);
+
+        
     }
 
 }
--- app/src/main/java/edu/temple/colorchangerapp/ColorAdapter.java
+++ app/src/main/java/edu/temple/colorchangerapp/ColorAdapter.java
@@ -1,26 +1,34 @@
 package edu.temple.colorchangerapp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ColorAdapter extends BaseAdapter {
 
-    public String color;
-    public ColorAdapter(Activity context, List<String> colors){
+    Context context;
+    ArrayList<String> colors;
+    public ColorAdapter(Activity context, ArrayList<String> colors){
+        this.context = context;
+        this.colors = colors;
     }
 
     @Override
     public int getCount(){
-        return 0;
+        return colors.size();
     }
 
     @Override
-    public String getItem(int i){
-        return color;
+    public Object getItem(int i){
+        return colors.get((i));
     }
 
     @Override
@@ -29,7 +37,25 @@ public class ColorAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int pos, View cv, ViewGroup vg){
-        return cv;
+    public View getView(int pos, View cv, ViewGroup p){
+
+        int colors[] = new int[9];
+        colors[0] = Color.BLACK;
+        colors[1] = Color.BLUE;
+        colors[2] = Color.YELLOW;
+        colors[3] = Color.MAGENTA;
+        colors[4] = Color.GREEN;
+        colors[5] = Color.CYAN;
+        colors[6] = Color.GRAY;
+        colors[7] = Color.RED;
+        colors[8] = Color.WHITE;
+        colors[9] = Color.DKGRAY;
+
+        TextView theview = new TextView(context);
+        theview.setText(getItem(pos).toString());
+        theview.setBackgroundColor(colors[pos]);
+
+
+        return theview;
     }
 }
